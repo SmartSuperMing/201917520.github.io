36401870e546f3a92d10058abff6088530aa6a81
mammon1113 <843120598@qq.com>
2017-10-10 13:47:06 +0800

    bash

--- demo.js
+++ demo.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 var fs = require('fs')
 var dirname = process.argv[2]
+var path = "./" + dirname
+if(fs.existsSync(path))
+{
+    console.log('existence address')
+    process.exit(1)
+}
+else{
 fs.mkdirSync("./" + dirname)
 process.chdir("./" + dirname)
 fs.mkdirSync('css')
@@ -9,3 +16,4 @@ fs.writeFileSync("./index.html"," <!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>"
 fs.writeFileSync("./css/style.css","h1{color: red;}")
 fs.writeFileSync("./js/main.js",'var string = "Hello World"\nalert(string)')
 process.exit(0)
+}
